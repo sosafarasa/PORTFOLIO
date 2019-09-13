0778c844ec1eb4389b29293e48116d6f551f296a
Agustina Sosa <agustinasf85@gmail.com>
2019-09-13 02:33:22 -0400

    Work in progress..

--- portfolio-fe/src/components/Content/Content.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Content = () => {
-    return null;
-}
-
-export default Content;
--- portfolio-fe/src/components/SideBar.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const SideBar = () => {
-    return null;
-}
-
-export default SideBar;
